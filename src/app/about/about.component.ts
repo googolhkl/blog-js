154357a5b43ab3cd47eb21031c652321987017a6
olo6525 <googolhkl@gmail.com>
2017-12-16 18:31:11 +0900

    about 추가

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ABOUT } from '../mocks/about';
 
 @Component({
   selector: 'app-about',
@@ -6,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./about.component.css']
 })
 export class AboutComponent implements OnInit {
+  about = ABOUT;
 
   constructor() { }
 
---
+++ src/app/mocks/about.ts
@@ -0,0 +1,6 @@
+import { About } from '../about/shared/about.model';
+
+export const ABOUT: About = {
+    photoUrl: 'https://s3.ap-northeast-2.amazonaws.com/apollon/about/me2.jpg',
+    likes: ['developer', 'software craftsmanship', 'photographer']
+};
