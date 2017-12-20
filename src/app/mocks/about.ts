24ddda115f37200df8fd147c43b979720206bfea
olo6525 <googolhkl@gmail.com>
2017-12-21 00:14:40 +0900

    About HTML 구현

--- src/app/mocks/about.ts
+++ src/app/mocks/about.ts
@@ -2,5 +2,5 @@ import { About } from '../about/shared/about.model';
 
 export const ABOUT: About = {
     photoUrl: 'https://s3.ap-northeast-2.amazonaws.com/apollon/about/me2.jpg',
-    likes: ['developer', 'software craftsmanship', 'photographer']
+    likes: ['develop', 'software craftsmanship', 'photography']
 };
