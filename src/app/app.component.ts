665a6a2fe07c185042644a64288e658289c37afe
olo6525 <googolhkl@gmail.com>
2018-01-22 22:05:38 +0900

    ga 제거

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
-import {Component, OnInit, AfterContentInit} from '@angular/core';
+import { Component } from '@angular/core';
 import {MatCheckboxModule} from '@angular/material/checkbox';
-import {Router, NavigationEnd} from '@angular/router';
 
 
 @Component({
@@ -8,21 +7,10 @@ import {Router, NavigationEnd} from '@angular/router';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements AfterContentInit {
+export class AppComponent {
   title = 'app';
   is_show_menu = false;
   hamberger = false;
-
-  constructor(private router: Router) {}
-
-  ngAfterContentInit() {
-    this.router.events.subscribe(event => {
-     if (event instanceof NavigationEnd) {
-       (<any>window).ga('set', 'page', event.urlAfterRedirects);
-       (<any>window).ga('send', 'pageview');
-      }
-    });
-  }
 
   toggleMenu(): void {
     this.is_show_menu = !this.is_show_menu;
